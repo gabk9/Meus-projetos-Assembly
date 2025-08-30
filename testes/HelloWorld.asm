d5f71f21f8b4843ecf4c5fffa550bfbec539128a
Gabriel Oliveira Miranda <giane.ga2010@gmail.com>
2025-08-30 18:33:53 -0300

    hello world assembly

--- testes/HelloWorld.asm
+++ testes/HelloWorld.asm
@@ -1,15 +1,15 @@
+global main
 extern printf
+
 section .data
     msg db "Hello World", 10, 0
 
 section .text
-    global main
-
 main:
-    sub rsp, 40
-    lea rcx, [msg]
-    call printf
-    add rsp, 40
-
-    mov eax, 0
-    ret
+    sub rsp, 40         ; reserves 40 bytes 
+    mov rcx, msg        ; moves the string address into rcx
+    xor rax, rax        ; RAX = 0
+    call printf         ; calls printf
+    add rsp, 40         ; free 40 bytes
+    xor eax, eax        ; return 0
+    ret                 ; terminated successfully
