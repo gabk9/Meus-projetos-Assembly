9c267da3582bf4b10fa7eaaef9301ac56cc0c9d9
Gabriel Oliveira Miranda <giane.ga2010@gmail.com>
2025-08-30 22:00:42 -0300

    Melhorei o helloworld

--- testes/HelloWorld.asm
+++ testes/HelloWorld.asm
@@ -2,14 +2,12 @@ global main
 extern printf
 
 section .data
-    msg db "Hello World", 10, 0
+    msg db "Hello, World!", 10, 0
 
 section .text
 main:
-    sub rsp, 40         ; reserves 40 bytes 
-    mov rcx, msg        ; moves the string address into rcx
-    xor rax, rax        ; RAX = 0
-    call printf         ; calls printf
-    add rsp, 40         ; free 40 bytes
-    xor eax, eax        ; return 0
-    ret                 ; terminated successfully
+    sub rsp, 40
+    lea rcx, [rel msg]
+    call printf
+    add rsp, 40
+    ret
