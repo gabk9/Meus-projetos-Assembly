0b76ff3b2381545b29ebfd8cd3ef3de114b3a263
Gabriel Oliveira <giane.ga2010@gmail.com>
2025-09-03 14:05:39 -0300

    Calculadora

--- testes/calculadora.asm
+++ testes/calculadora.asm
@@ -4,7 +4,7 @@ extern scanf
 extern system
 
 section .data
-    tittle db "======Calculator======", 10, 0
+    tittle db 10, "======Calculator======", 10, 0
     options db "[1] Multiplication", 10, "[2] Division", 10, "[3] Addition", 10, "[4] Subtraction" ,10, "[0] Exit program", 10, 0
 
     choice db "Choose an option: ", 0
@@ -14,11 +14,8 @@ section .data
     end db 10, "Terminating program, bye!!", 10, 0
     cmd db "pause", 0
 
-    n1 db "Type-in the 1st number: ", 0
-    n1_in db "%d", 0
-
-    n2 db "Type-in the 2nd number: ", 0
-    n2_in db "%d"
+    n db "Type-in a number: ", 0
+    n_in db "%d", 0
 
     div_or db 10, "or", 10, 0
     div_err db 10, "Cant divide by 0!!", 10, 0
@@ -35,7 +32,7 @@ section .bss
 
 section .text
 main:
-    sub rsp, 56
+    sub rsp, 40
 
 main_menu:
     lea rcx, [rel tittle]
@@ -44,42 +41,109 @@ main_menu:
     call printf
     lea rcx, [rel choice]
     call printf
+
     lea rcx, [rel choice_in]
-    lea eax, [rel op]
+    lea rdx, [rel op]
     call scanf
+
+    mov eax, dword [rel op]
 
     cmp eax, 1
     je multiplication_menu
-
     cmp eax, 2
     je division_menu
-
     cmp eax, 3
     je addition_menu
-
     cmp eax, 4
-    je main_menu
-
+    je Subtraction_menu
     cmp eax, 0
     je endif
-
     jmp invalid_option
 
+questions:
+    xor rsi, rsi
+
+    lea rcx, [rel n]
+    xor rax, rax
+    call printf
+
+    lea rcx, [rel n_in]
+    lea rdx, [rel num]
+    xor rax, rax
+    call scanf
+
+    lea rcx, [rel n]
+    xor rax, rax
+    call printf
+
+    lea rcx, [rel n_in]
+    lea rdx, [rel num+4]
+    xor rax, rax
+    call scanf
+    
+    ret
+
 Subtraction_menu:
+    sub rsp, 8
+    call questions
+    add rsp, 8
+
+    mov eax, dword [rel num]
+    mov ebx, dword [rel num+4]
+    sub eax, ebx
+    
+    sub rsp, 32
+    lea rcx, [rel min]
+    mov edx, dword [rel num]
+    mov r8d, dword [rel num+4]
+    mov r9d, eax
+    xor rax, rax
+    call printf ; TODO fix here
+    add rsp, 32
+
     jmp main_menu
-    ; TODO implement subtraction
 
 addition_menu:
+    sub rsp, 8
+    call questions
+    add rsp, 8
+
+    mov eax, dword [rel num]
+    mov ebx, dword [rel num+4]
+    add eax, ebx
+    
+    sub rsp, 32
+    lea rcx, [rel sum]
+    mov edx, dword [rel num]
+    mov r8d, dword [rel num+4]
+    mov r9d, eax
+    xor rax, rax
+    call printf ; TODO fix here
+    add rsp, 32
+
     jmp main_menu
-    ; TODO implement addition
 
 division_menu:
-    jmp main_menu
     ; TODO implement division
+    jmp main_menu
 
 multiplication_menu:
+    sub rsp, 8
+    call questions
+    add rsp, 8
+
+    mov eax, dword [rel num]
+    mov ebx, dword [rel num+4]
+    imul eax, ebx
+    
+    lea rcx, [rel multi]
+    mov edx, dword [rel num]
+    mov r8d, dword [rel num+4]
+    mov r9d, eax
+    xor rax, rax
+    call printf ; TODO fix here
+
     jmp main_menu
-    ; TODO implement multiplication
 
 invalid_option:
     lea rcx, [rel err]
@@ -96,6 +160,6 @@ endif:
     xor rax, rax
     call system
     
-    add rsp, 56
+    add rsp, 40
     xor eax, eax
     ret
