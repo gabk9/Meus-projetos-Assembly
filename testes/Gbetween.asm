6ebca9080bae10627d53705ab44b86ce709ec6cb
Gabriel Oliveira Miranda <giane.ga2010@gmail.com>
2025-09-05 22:25:21 -0300

    Conserter alguns codigos

--- testes/Gbetween.asm
+++ testes/Gbetween.asm
@@ -12,23 +12,25 @@ section .bss
 
 section .text
 main:
-    sub rsp, 32
-
+    sub rsp, 40
     xor rsi, rsi
-
+    
 .read_loop:
+    ; printf
+    sub rsp, 32
     lea rcx, [rel prompt]
-    xor rax, rax
     call printf
-
-    lea rcx, [rel prompt_in]
-    mov r8, rsi
-    imul r8, 4
+    add rsp, 32
+    
+    sub rsp, 32
+    mov rax, rsi
+    imul rax, 4
     lea rdx, [rel numbers]
-    add rdx, r8
-    xor rax, rax
+    add rdx, rax
+    lea rcx, [rel prompt_in]
     call scanf
-
+    add rsp, 32
+    
     inc rsi
     cmp rsi, 3
     jl .read_loop
@@ -36,22 +38,19 @@ main:
     mov eax, dword [rel numbers]
     mov ebx, dword [rel numbers+4]
     mov ecx, dword [rel numbers+8]
-
+    
     cmp eax, ebx
     cmovl eax, ebx
     cmp eax, ecx
     cmovl eax, ecx
 
-    sub rsp, 8
-    mov dword [rsp], eax
+    mov dword [rsp + 32], eax
     lea rcx, [rel msg]
     mov edx, dword [rel numbers]
-    mov r8d, dword [rel numbers+4]
+    mov r8d, dword [rel numbers+4] 
     mov r9d, dword [rel numbers+8]
-    xor rax, rax
     call printf
-    add rsp, 8
 
-    add rsp, 32
+    add rsp, 40
     xor eax, eax
     ret
--- testes/calculadora.asm
+++ testes/calculadora.asm
@@ -4,7 +4,7 @@ extern scanf
 extern system
 
 section .data
-    tittle db 10, "======Calculator======", 10, 0
+    tittle db "======Calculator======", 10, 0
     options db "[1] Multiplication", 10, "[2] Division", 10, "[3] Addition", 10, "[4] Subtraction" ,10, "[0] Exit program", 10, 0
 
     choice db "Choose an option: ", 0
@@ -13,6 +13,7 @@ section .data
     err db 10, "Error: Operation not permitted!", 10, 0
     end db 10, "Terminating program, bye!!", 10, 0
     cmd db "pause", 0
+    cmd2 db "cmd.exe /c cls", 0
 
     n db "Type-in a number: ", 0
     n_in db "%d", 0
@@ -25,6 +26,7 @@ section .data
     division2 db 10, "%d / %d = %d with remainder: %d", 10, 0
     sum db 10, "%d + %d = %d", 10, 0 
     min db 10, "%d - %d = %d", 10, 0
+    endline db 10, 0
 
 section .bss
     num1 resd 1 
@@ -36,16 +38,26 @@ main:
     sub rsp, 40
 
 main_menu:
+    sub rsp, 32
     lea rcx, [rel tittle]
     call printf
+    add rsp, 32
+
+    sub rsp, 32
     lea rcx, [rel options]
     call printf
+    add rsp, 32
+
+    sub rsp, 32
     lea rcx, [rel choice]
     call printf
+    add rsp, 32
 
+    sub rsp, 32
     lea rcx, [rel choice_in]
     lea rdx, [rel op]
     call scanf
+    add rsp, 32
 
     mov eax, dword [rel op]
 
@@ -62,35 +74,34 @@ main_menu:
     jmp invalid_option
 
 questions:
-    sub rsp, 8
+    sub rsp, 32
     lea rcx, [rel n]
-    xor rax, rax
     call printf
+    add rsp, 32
 
+    sub rsp, 32
     lea rcx, [rel n_in]
     lea rdx, [rel num1]
-    xor rax, rax
     call scanf
+    add rsp, 32
 
+    sub rsp, 32
     lea rcx, [rel n]
-    xor rax, rax
     call printf
+    add rsp, 32
 
+    sub rsp, 32
     lea rcx, [rel n_in]
     lea rdx, [rel num2]
-    xor rax, rax
     call scanf
-
-    add rsp, 8
+    add rsp, 32
     ret
 
 Subtraction_menu:
-    sub rsp, 8
     call questions
-    add rsp, 8
 
     mov eax, dword [rel num1]
-    mov ebx, dword [rel num1]
+    mov ebx, dword [rel num2]
     sub eax, ebx
     
     sub rsp, 32
@@ -98,30 +109,29 @@ Subtraction_menu:
     mov edx, dword [rel num1]
     mov r8d, dword [rel num2]
     mov r9d, eax
-    xor rax, rax
-    call printf ;! DOES NOT WORK PROPERLY
+    call printf
     add rsp, 32
 
+    call newline
     jmp main_menu
 
 addition_menu:
-    sub rsp, 8
     call questions
-    add rsp, 8
 
     mov eax, dword [rel num1]
     mov ebx, dword [rel num2]
     add eax, ebx
     
-    sub rsp, 8
+    sub rsp, 32
     lea rcx, [rel sum]
     mov edx, dword [rel num1]
     mov r8d, dword [rel num2]
     mov r9d, eax
     xor rax, rax
-    call printf ;! DOES NOT WORK PROPERLY
-    add rsp, 8
+    call printf
+    add rsp, 32
 
+    call newline
     jmp main_menu
 
 division_menu1:
@@ -129,9 +139,7 @@ division_menu1:
     jmp main_menu
 
 division_menu2:
-    sub rsp, 8
     call questions
-    add rsp, 8
 
     mov eax, [rel num1]
     cdq
@@ -140,63 +148,81 @@ division_menu2:
     jz is_zero
     idiv ebx
 
-    sub rsp, 8    
+    sub rsp, 32
+    mov [rsp + 32], edx
     lea rcx, [rel division2]
-    mov edx, dword [rel num1]
-    mov r8d, dword [rel num2] 
-    mov r9d, eax
-    mov r10d, edx
-    xor rax, rax
-    call printf ;! DOES NOT WORK PROPERLY
-    add rsp, 8
+    mov edx, [rel num1]
+    mov r8d, [rel num2]
+    mov r9d, eax 
+    call printf
+    add rsp, 32
 
+    call newline
     jmp main_menu
 
 is_zero:
-    sub rsp, 8
+    sub rsp, 32
     lea rcx, [rel div_err]
     call printf
 
     lea rcx, [rel cmd]
     xor rax, rax
     call system
-    add rsp, 8
+    add rsp, 32
 
+    call newline
     jmp main_menu
 
 multiplication_menu:
-    sub rsp, 8
     call questions
-    add rsp, 8
 
     mov eax, dword [rel num1]
     mov ebx, dword [rel num2]
     imul eax, ebx
     
+    sub rsp, 32
     lea rcx, [rel multi]
     mov edx, dword [rel num1]
     mov r8d, dword [rel num2]
     mov r9d, eax
     xor rax, rax
-    call printf ;! DOES NOT WORK PROPERLY
+    call printf
+    add rsp, 32
 
+    call newline
     jmp main_menu
+
+newline:
+    sub rsp, 32
+    lea rcx, [rel endline]
+    xor rax, rax
+    call printf
+    add rsp, 32
+    ret
 
 invalid_option:
+    sub rsp, 32
     lea rcx, [rel err]
     call printf
-    lea rcx, [rel cmd]
-    xor rax, rax
-    call system
-    jmp main_menu
+    add rsp, 32
 
+    sub rsp, 32
+    lea rcx, [rel cmd]
+    call system
+    add rsp, 32
+
+    jmp main_menu
 endif:
+    sub rsp, 32
     lea rcx, [rel end]
     call printf
+    add rsp, 32
+
+    sub rsp, 32
     lea rcx, [rel cmd]
-    xor rax, rax
     call system
-    
+    add rsp, 32
+
     add rsp, 40
     xor eax, eax
     ret
